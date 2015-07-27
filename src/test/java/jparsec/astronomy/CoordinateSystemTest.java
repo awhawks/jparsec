333439f07036becd8226ed5369482308763e86a3
Carlo Dapor <catull@gmail.com>
2015-07-27 21:12:42 +0200

    intermediate

--- src/test/java/jparsec/astronomy/CoordinateSystemTest.java
+++ src/test/java/jparsec/astronomy/CoordinateSystemTest.java
@@ -14,9 +14,13 @@ import jparsec.time.AstroDate;
 import jparsec.time.TimeElement;
 import jparsec.util.JPARSECException;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
 public class CoordinateSystemTest {
     /**
      * For unit testing only.
+     *
      * @param args Not used.
      */
     public static void main(String[] args) {
@@ -66,23 +70,25 @@ public class CoordinateSystemTest {
             System.out.println("b:  " + Functions.formatDEC(loc_out.getLatitude()));
             // Very nice since input position has an uncertainty above 20 mas.
 
-/*			loc_out = CoordinateSystem.galacticToEquatorial(loc_out, timef, observer, eph);
-			System.out.println("RA:  " + Functions.formatRA(loc_out.getLongitude()));
-			System.out.println("DEC: " + Functions.formatDEC(loc_out.getLatitude()));
-*/
+            /*
+            loc_out = CoordinateSystem.galacticToEquatorial(loc_out, timef, observer, eph);
+            System.out.println("RA:  " + Functions.formatRA(loc_out.getLongitude()));
+            System.out.println("DEC: " + Functions.formatDEC(loc_out.getLatitude()));
+            */
 
-/*			double obliquity = Obliquity.trueObliquity(Functions.toCenturies(Constant.J2000+12.5*365.25), eph.ephemMethod);
-			System.out.println(obliquity*Constant.RAD_TO_DEG);
-			loc_out = CoordinateSystem.eclipticToEquatorial(new LocationElement(
-					28.02 * Constant.DEG_TO_RAD, 33.35 * Constant.DEG_TO_RAD, 1.0
-					), obliquity, true);
-			System.out.println("RA:  " + Functions.formatRA(loc_out.getLongitude()));
-			System.out.println("DEC: " + Functions.formatDEC(loc_out.getLatitude()));
+            /*
+            double obliquity = Obliquity.trueObliquity(Functions.toCenturies(Constant.J2000+12.5*365.25), eph.ephemMethod);
+            System.out.println(obliquity*Constant.RAD_TO_DEG);
+            loc_out = CoordinateSystem.eclipticToEquatorial(new LocationElement(
+                    28.02 * Constant.DEG_TO_RAD, 33.35 * Constant.DEG_TO_RAD, 1.0
+                    ), obliquity, true);
+            System.out.println("RA:  " + Functions.formatRA(loc_out.getLongitude()));
+            System.out.println("DEC: " + Functions.formatDEC(loc_out.getLatitude()));
 
-			loc_out = CoordinateSystem.equatorialToEcliptic(loc_out, obliquity, true);
-			System.out.println("lon: " + Functions.formatAngleAsDegrees(loc_out.getLongitude(), 2));
-			System.out.println("lat: " + Functions.formatAngleAsDegrees(loc_out.getLatitude(), 2));
-*/
+            loc_out = CoordinateSystem.equatorialToEcliptic(loc_out, obliquity, true);
+            System.out.println("lon: " + Functions.formatAngleAsDegrees(loc_out.getLongitude(), 2));
+            System.out.println("lat: " + Functions.formatAngleAsDegrees(loc_out.getLatitude(), 2));
+            */
 
             eph.frame = EphemerisElement.FRAME.FK4;
             eph.equinox = EphemerisElement.EQUINOX_OF_DATE;
@@ -90,18 +96,46 @@ public class CoordinateSystemTest {
             eph.targetBody = Target.TARGET.JUPITER;
             EphemElement ephem1 = Ephem.getEphemeris(time, observer, eph, false);
             LocationElement loc1 = CoordinateSystem.equatorialToGalactic(ephem1.getEquatorialLocation(), time, observer, eph);
-            System.out.println("Jupiter, equ: "+ephem1.getEquatorialLocation().toStringAsEquatorialLocation());
-            System.out.println("Jupiter, gal: "+loc1.toString());
-            System.out.println("Jupiter, equ: "+CoordinateSystem.galacticToEquatorial(loc1, time, observer, eph).toStringAsEquatorialLocation());
+            System.out.println("Jupiter, equ: " + ephem1.getEquatorialLocation().toStringAsEquatorialLocation());
+            System.out.println("Jupiter, gal: " + loc1.toString());
+            System.out.println("Jupiter, equ: " + CoordinateSystem.galacticToEquatorial(loc1, time, observer, eph).toStringAsEquatorialLocation());
             eph.equinox = Constant.J2000;
             EphemElement ephem2 = Ephem.getEphemeris(time, observer, eph, false);
             LocationElement loc2 = CoordinateSystem.equatorialToGalactic(ephem2.getEquatorialLocation(), time, observer, eph);
-            System.out.println("Jupiter, equ: "+ephem2.getEquatorialLocation().toStringAsEquatorialLocation());
-            System.out.println("Jupiter, gal: "+loc2.toString());
-            System.out.println("Jupiter, equ: "+CoordinateSystem.galacticToEquatorial(loc2, time, observer, eph).toStringAsEquatorialLocation());
-        } catch (JPARSECException ve)
-        {
+            System.out.println("Jupiter, equ: " + ephem2.getEquatorialLocation().toStringAsEquatorialLocation());
+            System.out.println("Jupiter, gal: " + loc2.toString());
+            System.out.println("Jupiter, equ: " + CoordinateSystem.galacticToEquatorial(loc2, time, observer, eph).toStringAsEquatorialLocation());
+        } catch (JPARSECException ve) {
             JPARSECException.showException(ve);
         }
+    }
+
+    @Test
+    public void testLocationElement () throws Exception {
+        CityElement city = City.findCity("Madrid");
+        ObserverElement observer = ObserverElement.parseCity(city);
+        EphemerisElement eph = new EphemerisElement(
+            Target.TARGET.NOT_A_PLANET,
+            EphemerisElement.COORDINATES_TYPE.APPARENT,
+            EphemerisElement.EQUINOX_OF_DATE,
+            EphemerisElement.TOPOCENTRIC,
+            EphemerisElement.REDUCTION_METHOD.IAU_2006,
+            EphemerisElement.FRAME.FK5);
+
+        LocationElement loc = new LocationElement(0, 0, 1);
+        AstroDate astrof = new AstroDate(2000, AstroDate.JANUARY, 1, 12, 0, 0);
+        TimeElement timef = new TimeElement(astrof, TimeElement.SCALE.TERRESTRIAL_TIME);
+
+        LocationElement toEquatorial = CoordinateSystem.galacticToEquatorial(loc, timef, observer, eph);
+        assertEquals(Functions.formatRA(toEquatorial.getLongitude()), "17h 45m 37.1991s");
+        assertEquals(Functions.formatDEC(toEquatorial.getLatitude()), "-28 56 10.22");
+        // Should be 17 45 37.19  -28 56 10.22
+
+        LocationElement toGalactical = CoordinateSystem.equatorialToGalactic(toEquatorial, timef, observer, eph);
+        System.out.println("l:   " + Functions.formatDEC(toGalactical.getLongitude()));
+        System.out.println("b:   " + Functions.formatDEC(toGalactical.getLatitude()));
+
+        assertEquals(Functions.formatRA(toGalactical.getLongitude()), "0");
+        assertEquals(Functions.formatRA(toGalactical.getLatitude()), "0");
     }
 }
