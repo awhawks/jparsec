26a46d18862f190831a5d6842c9b9d99fc038d1b
Carlo Dapor <catull@gmail.com>
2015-11-06 20:51:10 +0100

    Implemented Coptic and Ethiopic calendars, both are immutable.

--- src/test/java/jparsec/time/calendar/CopticTest.java
+++ src/test/java/jparsec/time/calendar/CopticTest.java
@@ -6,15 +6,18 @@ public class CopticTest {
      *
      * @param args Not used.
      */
-    public static void main(String args[]) {
+    public static void main(final String args[]) {
         System.out.println("Coptic test");
 
-        int jd = 2451545;
+        double jd = 2451545.5;
+        long fixed = 730120;
         Coptic h = new Coptic(jd);
-        System.out.println("JD " + jd + " = " + h.year + '/' + h.month + '/' + h.day);
+        System.out.println("JD " + h.julianDate + ' ' + h.fixed + " = " + h);
+
         Coptic h2 = new Coptic(h.year, h.month, h.day);
-        System.out.println("JD " + h2.toJulianDay() + " = " + h2.year + '/' + h2.month + '/' + h2.day);
-        System.out.println(Calendar.nameFromMonth(h2.month, Coptic.MONTH_NAMES));
-        System.out.println(Calendar.nameFromDayOfWeek(Calendar.dayOfWeekFromFixed(h2.toFixed()), Coptic.DAY_OF_WEEK_NAMES));
+        System.out.println("JD " + h2.julianDate + ' ' + h2.fixed + " = " + h2);
+
+        //System.out.println(Calendar.nameFromMonth(h2.month, Coptic.MONTH_NAMES));
+        //System.out.println(Calendar.nameFromDayOfWeek(Calendar.dayOfWeekFromFixed(h2.fixed), Coptic.DAY_OF_WEEK_NAMES));
     }
 }
