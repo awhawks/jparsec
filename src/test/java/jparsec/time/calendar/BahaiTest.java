53c7dda429df05f1efb08c8ab539c137507bb67d
Tomás Alonso <talonsoalbi@gmail.com>
2015-12-03 18:23:21 +0100

    Comment fix for Bahai, value changed when testing and forgot to set it back to correct value

--- src/test/java/jparsec/time/calendar/BahaiTest.java
+++ src/test/java/jparsec/time/calendar/BahaiTest.java
@@ -7,7 +7,7 @@ public class BahaiTest {
         new Gregorian(2015,  3, 20).julianDate, // Julian day 2457101.5 is Bahai 1/9/19/19/19 (VE - 1 day)
         new Gregorian(2015,  3, 21).julianDate, // Julian day 2457102.5 is Bahai 1/10/1/1/1 (Vernal Equinox)
         new Gregorian(2015,  4,  9).julianDate, // Julian day 2457121.5 is Bahai 1/10/1/2/1 (VE + 1 Vahid)
-        new Gregorian(2205,  3, 1).julianDate, // Julian day 2526498.5 is Bahai 2/1/1/1/1
+        new Gregorian(2205,  3, 21).julianDate, // Julian day 2526498.5 is Bahai 2/1/1/1/1
         new Gregorian(2015,  12, 3).julianDate,
     };
 
@@ -17,7 +17,7 @@ public class BahaiTest {
         new Gregorian(2015,  3, 20).fixed, // Fixed day 735677 is Bahai 1/9/19/19/19 (VE - 1 day)
         new Gregorian(2015,  3, 21).fixed, // Fixed day 735678 is Bahai 1/10/1/1/1 (Vernal Equinox)
         new Gregorian(2015,  4,  9).fixed, // Fixed day 735697 is Bahai 1/10/1/2/1 (VE + 1 Vahid)
-        new Gregorian(2205,  3, 1).fixed, // Fixed day 805074 is Bahai 2/1/1/1/1
+        new Gregorian(2205,  3, 21).fixed, // Fixed day 805074 is Bahai 2/1/1/1/1
         new Gregorian(2015,  12, 3).fixed,
     };
 
