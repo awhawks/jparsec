e32ac277622254315c534ace5fe0e1d39033be8f
Tomás Alonso Albi <talonsoalbi@gmail.com>
2016-01-06 16:53:59 +0100

    Sky rendering did not show planets up to year 3000 B.C., only reached 1350 B.C.

--- src/main/java/jparsec/graph/chartRendering/RenderSky.java
+++ src/main/java/jparsec/graph/chartRendering/RenderSky.java
@@ -785,7 +785,7 @@ public class RenderSky
 		g.setStroke(JPARSECStroke.STROKE_DEFAULT_LINE_THIN);
         drawConstelNames();
 
-		if (jd > 1228000.5 && jd < 2817057.5)
+		if (jd > 625296.5 && jd < 2817057.5)
 		{
 			if (labels.size() > 0) {
 				drawLabels();
@@ -967,8 +967,7 @@ public class RenderSky
 			EphemerisElement sun_eph = new EphemerisElement(TARGET.SUN, projection.eph.ephemType,
 					EphemerisElement.EQUINOX_OF_DATE, projection.eph.isTopocentric, projection.eph.ephemMethod,
 					projection.eph.frame, projection.eph.algorithm);
-			sun_eph.correctForEOP = false;
-			sun_eph.correctForPolarMotion = false;
+			sun_eph.optimizeForSpeed();
 			render.planetRender.ephemSun = Ephem.getEphemeris(projection.time, projection.obs, sun_eph, false);
 			render.planetRender.telescope = render.telescope;
 	        if (forcedFOV) {
