e1c2b3372c63834c1c15a908dc2b0654972800bf
Carlo Dapor <catull@gmail.com>
2016-08-09 18:51:52 +0200

    Use constants HinduOldSolar.ARYA_SOLAR_MONTH and HinduOldSolar.ARYA_SOLAR_YEAR instead of magic values.

--- src/test/java/jparsec/time/calendar/HinduOldLunarTest.java
+++ src/test/java/jparsec/time/calendar/HinduOldLunarTest.java
@@ -16,5 +16,13 @@ public class HinduOldLunarTest {
         System.out.println(Calendar.nameFromMonth(h2.month, HinduOldLunar.MONTH_NAMES));
         System.out.println(Calendar.nameFromDayOfWeek(Calendar.dayOfWeekFromFixed(h2.toFixed()), HinduOldLunar.DAY_OF_WEEK_NAMES));
         System.out.println("(from sunrise)");
+
+        long fixed = HinduOldLunar.toFixed(3570, 11, true, 19);
+        h = new HinduOldLunar(1892731.5); // 171307
+        long fixed2 = h.toFixed(); // 1892760.5 // 171336
+
+        fixed = HinduOldLunar.toFixed(4940, 1, true, 13);
+        h = new HinduOldLunar(2392825.5); // 671401
+        fixed2 = h.toFixed(); // 2392855.5 // 671431
     }
 }
